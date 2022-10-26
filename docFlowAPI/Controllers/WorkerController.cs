e040a68a785ae2e6448271b2e0c9e00e390421fa
Dmitrii Kriazhev <kr.dmitry@mail.ru>
2022-10-26 23:01:42 +0800

    Получилось составить одну ручку

---
+++ docFlowAPI/Controllers/WorkerController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using System.Data;
+
+namespace docFlowAPI.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkerController : ControllerBase {
+        private readonly IConfiguration _configuration;
+        public WorkerController(IConfiguration configuration) {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get() {
+            string query = @"select * from get_workers()";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("docFlow");
+            NpgsqlDataReader myReader;
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource)) {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon)) {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ docFlowAPI/Models/Worker.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoApi.Models 
+{
+    public class workers
+    {
+        public int WorkerId { get; set; }
+        public string WorkerFullname { get; set; }
+        public int JobId { get; set; }
+        public string Job { get; set; }
+        public bool IsClassroomTeacher { get; set; }
+
+    }
+}
