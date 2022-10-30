51dedaceecf96fb219d6a7ededa1f1509e829c61
Dmitrii Kriazhev <kr.dmitry@mail.ru>
2022-10-30 16:13:01 +0800

    Сделал запросы для workers

--- docFlowAPI/Controllers/WorkerController.cs
+++ docFlowAPI/Controllers/WorkerController.cs
@@ -2,13 +2,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
 using System.Data;
+using TodoApi.Models;
 
 namespace docFlowAPI.Controllers {
     [Route("api/[controller]")]
     [ApiController]
-    public class WorkerController : ControllerBase {
+    public class WorkersController : ControllerBase {
         private readonly IConfiguration _configuration;
-        public WorkerController(IConfiguration configuration) {
+        public WorkersController(IConfiguration configuration) {
             _configuration = configuration;
         }
 
@@ -23,6 +24,84 @@ namespace docFlowAPI.Controllers {
                 myCon.Open();
                 using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon)) {
                     myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Workers workers) {
+            string query = @"call add_worker(@WorkerFullname, @JobId, @IsClassroomTeacher)";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("docFlow");
+            NpgsqlDataReader myReader;
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource)) {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon)) {
+                    myCommand.Parameters.AddWithValue("@WorkerFullname", workers.WorkerFullname);
+                    myCommand.Parameters.AddWithValue("@JobId", workers.JobId);
+                    myCommand.Parameters.AddWithValue("@IsClassroomTeacher", workers.IsClassroomTeacher);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPut]
+        public JsonResult Put(Workers workers) {
+            string query = @"call update_worker(@WorkerId, @WorkerFullname, @JobId, @IsClassroomTeacher)";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("docFlow");
+            NpgsqlDataReader myReader;
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource)) {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon)) {
+                    myCommand.Parameters.AddWithValue("@WorkerId", workers.WorkerId);
+                    myCommand.Parameters.AddWithValue("@WorkerFullname", workers.WorkerFullname);
+                    myCommand.Parameters.AddWithValue("@JobId", workers.JobId);
+                    myCommand.Parameters.AddWithValue("@IsClassroomTeacher", workers.IsClassroomTeacher);
+                    try {
+                        myReader = myCommand.ExecuteReader();
+                    } catch (PostgresException ex) {
+                        Response.StatusCode = 500;
+                        return new JsonResult(ex.Message);
+                    }
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id) {
+            string query = @"call del_worker(@WorkerId)";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("docFlow");
+            NpgsqlDataReader myReader;
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource)) {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon)) {
+                    myCommand.Parameters.AddWithValue("@WorkerId", id);
+                    try {
+                        myReader = myCommand.ExecuteReader();
+                    } catch (PostgresException ex) {
+                        Response.StatusCode = 500;
+                        return new JsonResult(ex.Message);
+                    }
                     table.Load(myReader);
 
                     myReader.Close();
--- docFlowAPI/Models/Workers.cs
+++ docFlowAPI/Models/Workers.cs
@@ -1,12 +1,10 @@
 ﻿namespace TodoApi.Models 
 {
-    public class workers
+    public class Workers
     {
         public int WorkerId { get; set; }
-        public string WorkerFullname { get; set; }
+        public string? WorkerFullname { get; set; }
         public int JobId { get; set; }
-        public string Job { get; set; }
         public bool IsClassroomTeacher { get; set; }
-
     }
 }
