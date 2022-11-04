6a597a8994c57888d939006da0e486faed07d2fa
Дмитрий <kr.dmitry@mail.ru>
2022-11-04 17:12:08 +0800

    Переделал запрос авторизации

---
+++ docFlowAPI/Controllers/LogIn.cs
@@ -0,0 +1,53 @@
+﻿using docFlowAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using System.Data;
+using TodoApi.Models;
+
+namespace docFlowAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogInController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public LogInController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public JsonResult Post(LoginForm loginForm)
+        {
+            string query = @"select * from isRegistered(@Mail, @Password)";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("docFlow");
+            NpgsqlDataReader myReader;
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Mail", loginForm.mail);
+                    myCommand.Parameters.AddWithValue("@Password", loginForm.password);
+                    try
+                    {
+                        myReader = myCommand.ExecuteReader();
+                    }
+                    catch (PostgresException ex)
+                    {
+                        Response.StatusCode = 500;
+                        return new JsonResult(ex.Message);
+                    }
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ docFlowAPI/Models/LoginForm.cs
@@ -0,0 +1,8 @@
+﻿namespace docFlowAPI.Models
+{
+    public class LoginForm
+    {
+        public string? mail { get; set; }
+        public string? password { get; set; }
+    }
+}
